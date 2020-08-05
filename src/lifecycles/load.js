4bcaf862f3de58f5398b019c4c3311da765d0214
xiaolin <462441355@qq.com>
2020-08-05 23:45:38 +0800

    * 预加载应用

---
+++ src/lifecycles/load.js
@@ -0,0 +1,29 @@
+import { LOADING_SOURCE_CODE, NOT_BOOTSTRAPPED } from "../application/app.helpers";
+
+/**
+ * 数组函数
+ * @param {*} fns 
+ */
+function flattenFnArray(fns) {
+    fns = Array.isArray(fns) ? fns : [fns];
+    // 通过promise链来链式调用  多个方法组合成一个方法
+    return (props) => fns.reduce((p, fn) => p.then(() => fn(props)), Promise.resolve());
+}
+
+
+export async function toLoadPromise(app) {
+    if (app.loadPromise) {
+        return app.loadPromise; //缓存机制
+    }
+    return (app.loadPromise = Promise.resolve().then(async () => {
+        app.status = LOADING_SOURCE_CODE;
+        let { bootstrap, mount, unmount } = await app.loadApp(app.customProps);
+        app.status = NOT_BOOTSTRAPPED; // 没有调用bootstrap方法
+        // 我希望将多个promise组合在一起 compose
+        app.bootstrap = flattenFnArray(bootstrap);
+        app.mount = flattenFnArray(mount);
+        app.unmount = flattenFnArray(unmount);
+        delete app.loadPromise;
+        return app;
+    }))
+}
--- src/navigations/reroute.js
+++ src/navigations/reroute.js
@@ -1,5 +1,6 @@
 import { started } from '../start.js'
 import { getAppChanges } from "../application/app.js";
+import { toLoadPromise } from '../lifecycles/load.js';
 
 
 // 核心应用处理方法
@@ -8,7 +9,6 @@ export function reroute() {
     //  需要获取要被挂载的应用
     //  哪些应用需要被卸载
     const { appsToLoad, appsToMount, appsToUnmount } = getAppChanges()
-    console.log(appsToLoad, appsToMount, appsToUnmount)
     // start方法调用时是同步的，但是加载流程是异步饿
     if (started) {
         // app装载
@@ -18,8 +18,11 @@ export function reroute() {
         return loadApps()
     }
     async function loadApps() { // 预加载应用
+        let apps = await Promise.all(appsToLoad.map(toLoadPromise)); // 就是获取到bootstrap,mount和unmount方法放到app上
+        console.log(apps)
     }
     async function performAppChanges() { // 根据路径来装载应用
+        
     }
 }
 
