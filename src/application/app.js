b7bf34c522e5ee7823bca76826ca21caed51e8ee
xiaolin <462441355@qq.com>
2020-08-05 22:49:39 +0800

    * 定义生命周期

--- src/application/app.js
+++ src/application/app.js
@@ -1,3 +1,22 @@
-export function registerApplication() {
-    
+
+/**
+ * 微前端注册
+ * @param {*} appName 应用名称
+ * @param {*} loadApp 加载的应用
+ * @param {*} activeWhen 当激活时会调用
+ * @param {*} customProps 自定义参数
+ */
+
+// 用来存放所有的应用
+const apps = []
+
+// 维护应用所有状态 状态机
+export function registerApplication(appName, loadApp, activeWhen, customProps) {
+    apps.push({ // 注册应用
+        name: appName,
+        loadApp,
+        activeWhen,
+        customProps
+    })
+    // vue 一系列的生命周期
 }
--- src/start.js
+++ src/start.js
@@ -1,3 +1,3 @@
 export function start() {
-
+    // 挂载应用
 }
