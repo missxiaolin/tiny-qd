96d7eedf107fe2a02614d918ec8f2b6e2bfbf953
xiaolin <462441355@qq.com>
2020-08-09 22:31:14 +0800

    * 修改目录结构

--- src/single-spa.js
+++ src/single-spa.js
@@ -1,4 +1,4 @@
 export { registerApplication } from './application/app.js'
 export { start } from './start.js'
 
-export { singleSpaVue } from './vue/index'
+export { singleSpaVue } from './sub/index'
