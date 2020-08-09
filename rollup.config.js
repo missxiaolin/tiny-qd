82e0f84dd6b8baa1dcd26ff08d3821df9495f777
xiaolin <462441355@qq.com>
2020-08-09 21:46:09 +0800

    * 修改打包

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import serve from 'rollup-plugin-serve'
 export default {
     input: './src/single-spa.js',
     output: {
-        file: './lib/umd/single-spa.js',
+        file: './lib/single-spa.js',
         format: 'umd',
         name: 'singleSpa',
         sourcemap: true
