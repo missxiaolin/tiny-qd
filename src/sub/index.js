4f251fa1caf52b97ee9d2c9efdabe29968ad9ad6
xiaolin <462441355@qq.com>
2020-08-10 10:10:19 +0800

    * 加载代码

--- src/sub/index.js
+++ src/sub/index.js
@@ -59,3 +59,78 @@ function bootstrap(opts) {
         return Promise.resolve()
     }
 }
+
+/**
+ * 加载
+ * @param {*} opts 
+ * @param {*} mountedInstances 
+ * @param {*} props 
+ */
+function mount(opts, mountedInstances, props) {
+    const instance = {}
+    return Promise.resolve().then(() => {
+        const appOptions = { ...opts.appOptions };
+        if (props.domElement && !appOptions.el) {
+            appOptions.el = props.domElement
+        }
+
+        let domEl
+        if (appOptions.el) {
+            if (typeof appOptions.el === "string") {
+                domEl = document.querySelector(appOptions.el)
+                if (!domEl) {
+                    throw Error(
+                        `If appOptions.el is provided to single-spa-vue, the dom element must exist in the dom. Was provided as ${appOptions.el}`
+                    );
+                }
+            } else {
+                domEl = appOptions.el
+                if (!domEl.id) {
+                    domEl.id = `single-spa-application:${props.name}`
+                }
+                appOptions.el = `#${CSS.escape(domEl.id)}`
+            }
+        } else {
+            const htmlId = `single-spa-application:${props.name}`
+            appOptions.el = `#${CSS.escape(htmlId)}`
+            domEl = document.getElementById(htmlId)
+            if (!domEl) {
+                domEl = document.createElement("div")
+                domEl.id = htmlId
+                document.body.appendChild(domEl)
+            }
+        }
+
+        appOptions.el = appOptions.el + " .single-spa-container"
+
+        // single-spa-vue@>=2 always REPLACES the `el` instead of appending to it.
+        // We want domEl to stick around and not be replaced. So we tell Vue to mount
+        // into a container div inside of the main domEl
+        if (!domEl.querySelector(".single-spa-container")) {
+            const singleSpaContainer = document.createElement("div")
+            singleSpaContainer.className = "single-spa-container"
+            domEl.appendChild(singleSpaContainer)
+        }
+
+        instance.domEl = domEl
+
+        if (!appOptions.render && !appOptions.template && opts.rootComponent) {
+            appOptions.render = h => h(opts.rootComponent)
+        }
+
+        if (!appOptions.data) {
+            appOptions.data = {}
+        }
+
+        appOptions.data = { ...appOptions.data, ...props }
+
+        instance.vueInstance = new opts.Vue(appOptions)
+        if (instance.vueInstance.bind) {
+            instance.vueInstance = instance.vueInstance.bind(instance.vueInstance)
+        }
+
+        mountedInstances[props.name] = instance
+
+        return instance.vueInstance
+    })
+}
